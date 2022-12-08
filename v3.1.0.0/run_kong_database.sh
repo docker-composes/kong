5433b2a22541cb4dc0337f239834b5b3ddd86113
koyeo <koyeo@qq.com>
2022-12-08 15:48:01 +0800

    init

---
+++ v3.1.0.0/run_kong_database.sh
@@ -0,0 +1,7 @@
+ docker run --rm -d --name kong-database \
+  --network=kong-net \
+  -p 5432:5432 \
+  -e "POSTGRES_USER=kong" \
+  -e "POSTGRES_DB=kong" \
+  -e "POSTGRES_PASSWORD=kongpass" \
+  -v "$PWD/data:/var/lib/postgresql/data" postgres:9.6
